e95e7fc6a70b4259cd318aba74071181e58a6fe4
LuizLimaDev <devluizlima@gmail.com>
2023-06-14 21:38:20 -0300

    adequacao do formato do database para o solicitado

--- noted.md
+++ noted.md
@@ -1,6 +1,12 @@
-
+# Estados globais 
+***useContext ou Redux?***
 - estado global autenticacao
 - utilizar estado do usuario para mostrar o nome no whats your mind
+- rota protegida (Dashboard) utilizando o estado do usuario
+
+# Post
 - timestamp e tempo de post
 - delete/edit post
+
+# General
 - animations 
--- src/Store/Reducers/posts.js
+++ src/Store/Reducers/posts.js
@@ -3,67 +3,67 @@ import { v4 as uuid } from 'uuid';
 
 const initialState = [
     {
-        "userId": uuid(),
-        "userName": "Victor",
         "id": uuid(),
+        "userName": "Victor",
+        "created_datetime": "datetime",
         "title": "My First Post at CodeLeap Network",
-        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        "content": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
     },
     {
-        "userId": uuid(),
-        "userName": "Victor",
         "id": uuid(),
+        "userName": "Victor",
+        "created_datetime": "datetime",
         "title": "My Second Post at CodeLeap Network",
-        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        "content": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
     },
     {
-        "userId": uuid(),
+        "id": uuid(),
         "userName": "Victor",
-        "id": uuid(),
+        "created_datetime": "datetime",
         "title": "My third Post at CodeLeap Network",
-        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+        "content": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
     },
     {
-        "userId": uuid(),
-        "userName": "Luiz",
         "id": uuid(),
+        "userName": "Luiz",
+        "created_datetime": "datetime",
         "title": "et ea vero quia laudantium autem",
-        "body": "delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi"
+        "content": "delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi"
     },
     {
-        "userId": uuid(),
-        "userName": "Luiz",
         "id": uuid(),
+        "userName": "Luiz",
+        "created_datetime": "datetime",
         "title": "in quibusdam tempore odit est dolorem",
-        "body": "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio"
+        "content": "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio"
     },
     {
-        "userId": uuid(),
+        "id": uuid(),
         "userName": "Luiz",
-        "id": uuid(),
+        "created_datetime": "datetime",
         "title": "dolorum ut in voluptas mollitia et saepe quo animi",
-        "body": "aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti reiciendis voluptatem eius rerum\nsit cumque quod eligendi laborum minima\nperferendis recusandae assumenda consectetur porro architecto ipsum ipsam"
+        "content": "aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti reiciendis voluptatem eius rerum\nsit cumque quod eligendi laborum minima\nperferendis recusandae assumenda consectetur porro architecto ipsum ipsam"
     },
     {
-        "userId": uuid(),
-        "userName": "Tiffany",
         "id": uuid(),
+        "userName": "Tiffany",
+        "created_datetime": "datetime",
         "title": "asperiores ea ipsam voluptatibus modi minima quia sint",
-        "body": "repellat aliquid praesentium dolorem quo\nsed totam minus non itaque\nnihil labore molestiae sunt dolor eveniet hic recusandae veniam\ntempora et tenetur expedita sunt"
+        "content": "repellat aliquid praesentium dolorem quo\nsed totam minus non itaque\nnihil labore molestiae sunt dolor eveniet hic recusandae veniam\ntempora et tenetur expedita sunt"
     },
     {
-        "userId": uuid(),
-        "userName": "Tiffany",
         "id": uuid(),
+        "userName": "Tiffany",
+        "created_datetime": "datetime",
         "title": "dolor sint quo a velit explicabo quia nam",
-        "body": "eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse"
+        "content": "eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse"
     },
     {
-        "userId": uuid(),
-        "userName": "Tiffany",
         "id": uuid(),
+        "userName": "Tiffany",
+        "created_datetime": "datetime",
         "title": "maxime id vitae nihil numquam",
-        "body": "veritatis unde neque eligendi\nquae quod architecto quo neque vitae\nest illo sit tempora doloremque fugit quod\net et vel beatae sequi ullam sed tenetur perspiciatis"
+        "content": "veritatis unde neque eligendi\nquae quod architecto quo neque vitae\nest illo sit tempora doloremque fugit quod\net et vel beatae sequi ullam sed tenetur perspiciatis"
     }
 ]
 
